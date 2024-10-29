b26cd47c974601fc619628bd64810140d10b5c49
ahmed <foudiahmed82@gmail.com>
2024-10-29 14:44:57 +0100

    first commit

---
+++ src/main/java/ahmed/foudi/itlens/ItLensApplication.java
@@ -0,0 +1,13 @@
+package ahmed.foudi.itlens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItLensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItLensApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ahmed/foudi/itlens/ItLensApplicationTests.java
@@ -0,0 +1,13 @@
+package ahmed.foudi.itlens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItLensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
