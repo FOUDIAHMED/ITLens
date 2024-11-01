3639a541e0a3744dc1511728b62aac70403566fe
ahmed <foudiahmed82@gmail.com>
2024-11-01 11:32:08 +0100

    Question entity

---
+++ src/main/java/ahmed/foudi/itlens/entities/Question.java
@@ -0,0 +1,28 @@
+package ahmed.foudi.itlens.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String question;
+
+    private int answerCount;
+
+    @OneToMany(mappedBy="owner", fetch = FetchType.EAGER)
+    private List<Answer> answers;
+
+    public Question() {}
+
+
+
+}
---
+++ src/main/java/ahmed/foudi/itlens/entities/QuestionType.java
@@ -0,0 +1,6 @@
+package ahmed.foudi.itlens.entities;
+
+public enum QuestionType {
+    SINGLE_CHOICE,
+    MULTIPLE_CHOICE,
+}
