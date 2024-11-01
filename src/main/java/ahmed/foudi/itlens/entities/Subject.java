d4c73f0e258d5bc5cc09254378ad348c8ad620c4
ahmed <foudiahmed82@gmail.com>
2024-11-01 11:29:37 +0100

    Survey entity

---
+++ src/main/java/ahmed/foudi/itlens/entities/Subject.java
@@ -0,0 +1,26 @@
+package ahmed.foudi.itlens.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @OneToMany(mappedBy = "parentSubject", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Subject> subSubjects;
+
+    // Self-referencing many-to-one relationship
+    @ManyToOne
+    @JoinColumn(name = "parent_subject_id")
+    private Subject parentSubject;
+}
